201c8e3c505ad1ad203eb850b52d7a9175ab49ac
Daniel Yu <daniel.yu@displayr.com>
2024-06-21 12:00:42 +0930

    test

--- provider/resources.go
+++ provider/resources.go
@@ -123,7 +123,7 @@ func Provider() tfbridge.ProviderInfo {
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
-		PluginDownloadURL: "https://github.com/Displayr/pulumi-signalfx-synthetics/releases",
+		PluginDownloadURL: "",
 		Description:       "A Pulumi package for creating and managing pulumi-signalfx-synthetics cloud resources.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
 		// For all available categories, see `Keywords` in
